055525c6b1caddabcace4b98df2b726d1aba15f2
goatsefl <swaroopd502@gmail.com>
2025-01-18 23:20:51 +0530

    Added information about the theme implementation

--- contextapi/src/App.js
+++ contextapi/src/App.js
@@ -9,6 +9,13 @@ export default function App() {
   const toggleBg = () => {
     setTheme(prev => !prev)
   }
+
+  // The hook below is rendered whenever theme variable is changed to dark or light
+  // The body's className will change to light, if true, else dark inside the useEffect hook
+  // For that reason, we added theme in our dependency array. As it changes and renders every time the value is changed.
+  // One of the main uses of useEffect is to be used when some elements of DOM element needs to be changed.
+  // When there are multiple places to apply the same effect, it'd be better to use Context API.
+  // In our case, the context API is UseInfoContext.js
   useEffect(() => {
     document.body.className = theme ? "light" : 'dark';
   }, [theme])
